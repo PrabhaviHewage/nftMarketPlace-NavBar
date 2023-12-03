363cb745a8b317c283d8672a98292f81f93cf064
PrabhaviHewage <prabhavihewage5@gmail.com>
2023-12-04 01:04:55 +0530

    first commit

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,18 @@ import React from "react";
 
 //INTERNAL IMPORT
 import Style from "../app/index.module.css";
-import { HeroSection, Service } from "../components/componentsindex";
+import {
+  HeroSection,
+  Service,
+  BigNFTSilder,
+} from "../components/componentsindex";
 
 const Home = () => {
   return (
     <div className={Style.homePage}>
       <HeroSection />
       <Service />
+      <BigNFTSilder />
     </div>
   );
 };
