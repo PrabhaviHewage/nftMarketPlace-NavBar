2c7481c00738ffabeca7c86e29bd0bd977efbfb9
PrabhaviHewage <prabhavihewage5@gmail.com>
2023-12-03 21:24:54 +0530

    first commit

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,13 @@ import React from "react";
 
 //INTERNAL IMPORT
 import Style from "../app/index.module.css";
-import { HeroSection } from "../components/componentsindex";
+import { HeroSection, Service } from "../components/componentsindex";
 
 const Home = () => {
   return (
     <div className={Style.homePage}>
       <HeroSection />
+      <Service />
     </div>
   );
 };
