58fc393e95d7e7e98e54d45369bac87573c4aa00
PrabhaviHewage <prabhavihewage5@gmail.com>
2023-12-03 16:42:32 +0530

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import "../app/globals.css";
+
+//INTERNAL IMPORT
+import { NavBar } from "@/components/componentsindex";
+
+const MyApp = ({ Component, pageProps }) => (
+<div>
+    <NavBar/>
+    <Component {...pageProps} />
+</div>
+
+);
+
+export default MyApp;
